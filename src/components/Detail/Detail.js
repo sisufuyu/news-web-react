5965c8e2673560e94f0e54962c90322384c9ca96
sisufuyu <sisu.fuyu@gmail.com>
2021-12-02 15:21:59 +0200

    Add files via upload

---
+++ src/components/Detail/Detail.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import { Card } from 'antd';
+import './Detail.css';
+class Detail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'title',
+            content: 'content'
+        }
+    }
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        axios.get('http://www.dell-lee.com/react/api/detail.json?id=' + id)
+            .then(res => {
+                let data = res.data.data;
+                this.setState(data);
+            })
+    }
+    render() {
+        return(
+            <Card title={this.state.title}>
+                <div className="detail-content" dangerouslySetInnerHTML={{__html: this.state.content}}></div>
+            </Card>
+        );
+    }
+}
+
+export default withRouter(Detail);
